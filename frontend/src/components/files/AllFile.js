aeb3c67cf699728d4d1ebd534d1b1a49c42054ee
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2019-04-13 00:07:00 -0700

    Update AllFile.js

--- frontend/src/components/files/AllFile.js
+++ frontend/src/components/files/AllFile.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import {
-    Layout, Menu, Avatar, Button, Icon
+    Layout, Menu, Avatar, Button, Icon, Card,Empty
 } from 'antd';
 
 import { Router, Route } from 'react-router-dom';
 
+const {Meta} = Card;
+const gridStyle = {
+    width: '33%',
+    textAlign: 'center',
+};
+
 
 class AllFile extends Component {
-
 
 
     render() {
@@ -16,7 +21,8 @@ class AllFile extends Component {
         return (
             <div className="AllFile">
 
-               All Files
+                <Empty />
+
             </div>
         );
     }
